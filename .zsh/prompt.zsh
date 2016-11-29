fce24314e9b0f6ca3422c548461739cedc703a3d
Mathias Jean Johansen <mathias@mjj.io>
2016-11-29 11:45:33 +0100

    Set prompt in `precmd`.

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,6 +1,9 @@
 local git_info='$(git_prompt_info)'
+
 ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[blue]%}(%{$reset_color%}%{$fg[cyan]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[blue]%})%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}*%{$reset_colors%}"  
 
-export PS1="%{$fg[blue]%}%~${git_info} %{$fg[yellow]%}%#%{%{$reset_color%}%} "
+precmd() {
+  PS1="%{$fg[blue]%}%~${git_info} %{$fg[yellow]%}%#%{%{$reset_color%}%} "
+}
