afec5be06d646ba939bab3628715254da3d5472b
Mathias Jean Johansen <mathias@mjj.io>
2016-12-19 07:41:05 +0100

    Add `chruby`.

--- .zsh/externals.zsh
+++ .zsh/externals.zsh
@@ -3,3 +3,8 @@ source ~/.env/bin/z/z.sh
 
 # FZF
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# chruby
+source /usr/local/opt/chruby/share/chruby/chruby.sh
+source /usr/local/opt/chruby/share/chruby/auto.sh
+chruby ruby-2.3.3
