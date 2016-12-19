51914467713617f007bb219dfc0b65d4c9d7c18e
Mathias Jean Johansen <mathias@mjj.io>
2016-12-19 07:40:49 +0100

    Add `docker-gc`.

--- .zsh/docker.zsh
+++ .zsh/docker.zsh
@@ -1,11 +1,15 @@
-idris () {
-  docker run -t -i majjoha/idris idris
-}
-
 blog () {
   docker run \
     -p 4000:4000 \
     -v $(pwd):/app \
     -d \
     majjoha/blog
+}
+
+docker-gc () {
+  docker run \
+    --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /etc:/etc \
+    spotify/docker-gc
 }
