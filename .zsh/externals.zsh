b2d313ef1da62b408b8ba6c8297c8eb3d11a88cf
Mathias Jean Johansen <mathias@mjj.io>
2017-02-24 19:55:13 +0100

    Source `chruby` if it exists, otherwise skip it.

--- .zsh/externals.zsh
+++ .zsh/externals.zsh
@@ -5,6 +5,8 @@ source ~/.env/bin/z/z.sh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # chruby
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
-chruby ruby-2.4.0
+if [ -d "/usr/local/opt/chruby/" ]; then
+  source /usr/local/opt/chruby/share/chruby/chruby.sh
+  source /usr/local/opt/chruby/share/chruby/auto.sh
+  chruby ruby-2.4.0
+fi
