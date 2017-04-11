5e416ac250f90f15342455abdcf15616a0978a22
Mathias Jean Johansen <mathias@mjj.io>
2017-04-11 16:14:40 +0200

    Rename bin folder to .bin.

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -26,20 +26,13 @@ export LC_CTYPE="utf-8"
 # Install Homebrew Cask applications in /Applications folder.
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-# Set GOPATH
-if [ $(hostname) = "libris" ]; then
-  export GOPATH=$HOME/Dropbox/Kode/Go
-else
-  export GOPATH=$HOME/work
-fi
-
 # Disallow tracking in Homebrew
 export HOMEBREW_NO_ANALYTICS=1
 
 export MYSQL=/usr/local/mysql/bin
 export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-export PATH=/usr/local/bin:~/.env/bin:/usr/local/share/npm/bin:$PATH
+export PATH=/usr/local/bin:~/.env/.bin:/usr/local/share/npm/bin:$PATH
 export PATH="/usr/local/heroku/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
@@ -47,7 +40,6 @@ export PATH="$HOME/.npm:$PATH"
 export PATH="$HOME/.nvm/bin:$PATH"
 export PATH="./node_modules/.bin:$PATH"
 export PATH=$PATH:$MYSQL
-export PATH=$GOPATH/bin:$PATH
 
 # Set path to Rust source code
 export RUST_SRC_PATH=/Users/mathias/Dropbox/Kode/Rust/rust/src
--- .zsh/externals.zsh
+++ .zsh/externals.zsh
@@ -1,5 +1,5 @@
 # Use z
-source ~/.env/bin/z/z.sh
+source ~/.env/.bin/z/z.sh
 
 # FZF
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
