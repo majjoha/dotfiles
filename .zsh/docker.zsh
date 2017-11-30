60e37568b972daf3f5b4e0006972c0d6dc017954
Mathias Jean Johansen <mathias@mjj.io>
2017-11-30 10:57:50 +0100

    Remove unused container alias.

--- .zsh/docker.zsh
+++ .zsh/docker.zsh
@@ -5,11 +5,3 @@ blog () {
     -d \
     majjoha/blog
 }
-
-docker-gc () {
-  docker run \
-    --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /etc:/etc \
-    spotify/docker-gc
-}
