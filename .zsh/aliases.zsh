ca39056c61c2127f9426612771e2c9c478918e2b
Mathias Jean Johansen <mathias@mjj.io>
2020-10-18 15:38:49 +0200

    Rebase automatically on `git pull`.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -105,7 +105,6 @@ alias gs="git status"
 alias gsa="git submodule add"
 alias gsrm="git rm"
 alias gsu="git submodule update --remote --jobs=4"
-alias gpr="git pull --rebase"
 
 # Rebuild Spotlight index
 alias reindex-spotlight="sudo mdutil -i on /"
