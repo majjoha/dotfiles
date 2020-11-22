34d3202cd401a2854c6696fe78d26c68df409adf
Mathias Jean Johansen <mathias@mjj.io>
2020-11-22 13:00:34 +0100

    Replace window key bindings with Spoon module.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,9 +1,40 @@
 local battery = require "battery"
-local caffeine = require "caffeine"
 local keybindings = require "keybindings"
-local reloader = require "reloader"
+
+-- Disable animation when repositioning windows
+hs.window.animationDuration = 0
+
+local hyper = {"ctrl", "alt"}
+
+hs.loadSpoon("SpoonInstall")
+
+local spoons = {
+  ReloadConfiguration = { start = true },
+  Caffeine = { start = true },
+  WindowHalfsAndThirds = {
+    hotkeys = {
+      left_half    = {hyper, "H" },
+      right_half   = {hyper, "L" },
+      bottom_half  = {hyper, "J"},
+      top_half     = {hyper, "K"},
+      top_right    = {hyper, "["},
+      bottom_right = {hyper, "'"},
+      top_left     = {hyper, ";"},
+      bottom_left  = {hyper, "/"},
+      max          = {hyper, "M" }
+    }
+  }
+}
+
+-- Load all spoons with their respective settings
+for plugin, configuration in pairs(spoons) do
+  spoon.SpoonInstall:andUse(plugin, configuration)
+end
+
+-- Start Caffeine on load
+spoon.Caffeine:setState(true)
 
 battery.init()
-caffeine.init()
 keybindings.init()
-reloader.init()
+
+hs.alert.show("Config loaded")
--- .hammerspoon/keybindings.lua
+++ .hammerspoon/keybindings.lua
@@ -1,67 +1,19 @@
 local mod = {}
 local hyper = {"ctrl", "alt"}
 
--- Disable animation when repositioning windows
-hs.window.animationDuration = 0
-
-hs.grid.MARGINX = 0
-hs.grid.MARGINY = 0
-hs.grid.GRIDWIDTH = 10
-hs.grid.GRIDHEIGHT = 10
-
 function mod.init()
-  -- Move to right half
-  hs.hotkey.bind(hyper, "H", function()
-    local win = hs.window.focusedWindow()
-    hs.grid.set(win, { x = 0, y = 0, w = 5, h = 10 }, win:screen())
+  -- Scroll up
+  hs.hotkey.bind(hyper, ".", function()
+    hs.eventtap.scrollWheel({0, 50}, {}, "pixel")
+  end, nil, function()
+    hs.eventtap.scrollWheel({0, 50}, {}, "pixel")
   end)
 
-  -- Move window to left half
-  hs.hotkey.bind(hyper, "L", function()
-    local win = hs.window.focusedWindow()
-    hs.grid.set(win, { x = 5, y = 0, w = 5, h = 10 }, win:screen())
-  end)
-
-  -- Full screen
-  hs.hotkey.bind(hyper, "M", function()
-    local win = hs.window.focusedWindow()
-    hs.grid.set(win, { x = 0, y = 0, w = 10, h = 10 }, win:screen())
-  end)
-
-  -- Move to bottom half
-  hs.hotkey.bind(hyper, "J", function()
-    local win = hs.window.focusedWindow()
-    hs.grid.set(win, { x = 0, y = 5, w = 10, h = 5 }, win:screen())
-  end)
-
-  -- Move to top half
-  hs.hotkey.bind(hyper, "K", function()
-    local win = hs.window.focusedWindow()
-    hs.grid.set(win, { x = 0, y = 0, w = 10, h = 5 }, win:screen())
-  end)
-
-  -- Move to quarter right top
-  hs.hotkey.bind(hyper, "[", function()
-    local win = hs.window.focusedWindow()
-    hs.grid.set(win, { x = 5, y = 0, w = 5, h = 5 }, win:screen())
-  end)
-
-  -- Move to quarter right bottom
-  hs.hotkey.bind(hyper, "'", function()
-    local win = hs.window.focusedWindow()
-    hs.grid.set(win, { x = 5, y = 5, w = 5, h = 5 }, win:screen())
-  end)
-
-  -- Move to quarter left top
-  hs.hotkey.bind(hyper, ";", function()
-    local win = hs.window.focusedWindow()
-    hs.grid.set(win, { x = 0, y = 0, w = 5, h = 5 }, win:screen())
-  end)
-
-  -- Move to quarter left bottom
-  hs.hotkey.bind(hyper, "/", function()
-    local win = hs.window.focusedWindow()
-    hs.grid.set(win, { x = 0, y = 5, w = 5, h = 5 }, win:screen())
+  -- Scroll down
+  hs.hotkey.bind(hyper, ",", function()
+    hs.eventtap.scrollWheel({0, -50}, {}, "pixel")
+  end, nil, function()
+    hs.eventtap.scrollWheel({0, -50}, {}, "pixel")
   end)
 
   -- Application hotkeys
@@ -75,6 +27,7 @@ function mod.init()
     t = "Kitty",
     z = "Zoom.us"
   }
+
   for key, app in pairs(charsToApps) do
     hs.hotkey.bind(hyper, key, function()
       hs.application.launchOrFocus(app)
