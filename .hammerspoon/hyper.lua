10f75a079de1933ac1423ca2404c98b240eae47f
Mathias Jean Johansen <mathias@mjj.io>
2020-12-09 17:58:59 +0100

    Restructure Hammerspoon configuration.

--- .hammerspoon/hyper.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "ctrl",
-  "alt"
-}
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,37 +1,45 @@
-local hyper = require "hyper"
-local keybindings = require "keybindings"
-
 -- Disable animation when repositioning windows
 hs.window.animationDuration = 0
 
 hs.loadSpoon("SpoonInstall")
 
-local spoons = {
-  ReloadConfiguration = { start = true },
-  Caffeine = { start = true },
-  WindowHalfsAndThirds = {
-    hotkeys = {
-      left_half    = {hyper, "H" },
-      right_half   = {hyper, "L" },
-      bottom_half  = {hyper, "J"},
-      top_half     = {hyper, "K"},
-      top_right    = {hyper, "["},
-      bottom_right = {hyper, "'"},
-      top_left     = {hyper, ";"},
-      bottom_left  = {hyper, "/"},
-      max          = {hyper, "M" }
-    }
-  }
-}
+local hyper = {"ctrl", "alt"}
 
 -- Load all spoons with their respective settings
-for plugin, configuration in pairs(spoons) do
+for plugin, configuration in pairs({
+  AppLauncher = {
+    hotkeys = {
+      c = "Calendar",
+      d = "Discord",
+      f = "Firefox Developer Edition",
+      n = "Notes",
+      p = "1Password 7",
+      r = "Reeder",
+      t = "Kitty",
+      z = "Zoom.us",
+    }
+  },
+  Caffeine = { start = true },
+  FnMate = {},
+  ReloadConfiguration = { start = true },
+  WindowHalfsAndThirds = {
+    hotkeys = {
+      left_half    = {hyper, "H"},
+      right_half   = {hyper, "L"},
+      bottom_half  = {hyper, "J"},
+      top_half     = {hyper, "K"},
+      top_right    = {hyper, "]"},
+      bottom_right = {hyper, "'"},
+      top_left     = {hyper, "["},
+      bottom_left  = {hyper, ";"},
+      max          = {hyper, "M"},
+    }
+  }
+}) do
   spoon.SpoonInstall:andUse(plugin, configuration)
 end
 
 -- Start Caffeine on load
 spoon.Caffeine:setState(true)
-
-keybindings.init()
 
 hs.alert.show("Config loaded")
