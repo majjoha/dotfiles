3b30f124084b33bdeaa3e768a01b45c02b15ad15
Mathias Jean Johansen <mathias@mjj.io>
2021-04-04 17:04:06 +0200

    Load `ghcup`.

--- .config/zsh/externals.zsh
+++ .config/zsh/externals.zsh
@@ -9,3 +9,6 @@ if [ -d "/usr/local/opt/chruby/" ]; then
   source /usr/local/opt/chruby/share/chruby/chruby.sh
   source /usr/local/opt/chruby/share/chruby/auto.sh
 fi
+
+# ghcup-env
+[ -f "/Users/mathias/.ghcup/env" ] && source "/Users/mathias/.ghcup/env"
