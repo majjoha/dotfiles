36d439344bb82b99348ecf7677e440fe95a218f7
Mathias Jean Johansen <mathias@mjj.io>
2021-04-09 14:48:04 +0200

    Set options for `FZF_DEFAULT_OPTS`.

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -42,3 +42,14 @@ export GPG_TTY=$(tty)
 
 # Show hidden files when searching
 export FZF_DEFAULT_COMMAND="rg --hidden"
+
+# Show the prompt at the top with inline info, place a border around FZF and
+# take up only 35 percent of the screen height. Display a preview of the file,
+# and set the colors to match the terminal theme
+export FZF_DEFAULT_OPTS="
+  --reverse \
+  --inline-info \
+  --height=35% \
+  --border \
+  --preview='head -$LINES {}'
+  --color=bg+:11,bg:0,border:8,spinner:7,hl:8,fg:7,prompt:8,header:8,info:12,pointer:7,marker:7,fg+:7,gutter:0,hl+:12"
