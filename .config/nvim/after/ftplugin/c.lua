6b561cdff335d33ece1d91313f168622ad927bc0
Mathias Jean Johansen <mathias@mjj.io>
2021-07-12 21:17:09 +0200

    Convert `c.vim` to Lua.

---
+++ .config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,5 @@
+-- Use `clang-format` for formatting
+vim.opt_local.formatprg = "clang-format"
+
+-- Use // for comments instead of /* ... */
+vim.opt_local.commentstring = "// %s"
--- .config/nvim/after/ftplugin/c.vim
+++
@@ -1,5 +0,0 @@
-" Use `clang-format` for formatting
-setlocal formatprg=clang-format
-
-" Use // for comments instead of /* ... */
-setlocal commentstring=//\ %s
