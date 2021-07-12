5fa9557c8db088c4b2c1a343e2293a2dad1a19f2
Mathias Jean Johansen <mathias@mjj.io>
2021-07-12 21:00:46 +0200

    Convert `appearance.vim` to Lua.

---
+++ .config/nvim/plugin/appearance.lua
@@ -0,0 +1,26 @@
+-- Set theme
+vim.g.colors_name = "base16"
+vim.opt.background = "dark"
+
+-- Enable relative line numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set color column to 80
+vim.opt.colorcolumn = "80"
+
+-- Always show the status line
+vim.opt.laststatus = 2
+
+vim.opt.fillchars = {
+  -- Blend vertical separator and line numbers
+  vert = " ",
+  -- Supress the `~` character at the end of the buffer
+  eob = " ",
+}
+
+-- Show the current mode
+vim.opt.showmode = true
+
+-- Do not display key strokes
+vim.opt.showcmd = false
