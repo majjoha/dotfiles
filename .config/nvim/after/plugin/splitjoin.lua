029fed1c6298fee572f08306d514e2bccf861448
Mathias Jean Johansen <mathias@mjj.io>
2021-07-12 22:14:11 +0200

    Convert `splitjoin.vim` to Lua.

---
+++ .config/nvim/after/plugin/splitjoin.lua
@@ -0,0 +1,7 @@
+-- Skip if `splitjoin` is not loaded
+if not vim.g.loaded_splitjoin then
+  return
+end
+
+vim.g.splitjoin_ruby_curly_braces = 0
+vim.g.splitjoin_ruby_hanging_args = 0
--- .config/nvim/after/plugin/splitjoin.vim
+++
@@ -1,7 +0,0 @@
-" Skip if `splitjoin` is not loaded
-if !exists("g:loaded_splitjoin")
-  finish
-endif
-
-let g:splitjoin_ruby_curly_braces = 0
-let g:splitjoin_ruby_hanging_args = 0
