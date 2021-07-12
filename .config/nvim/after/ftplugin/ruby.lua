1a7d369028785be40f1f9dcd486983583a073fb2
Mathias Jean Johansen <mathias@mjj.io>
2021-07-12 21:57:11 +0200

    Convert `ruby.vim` to Lua.

---
+++ .config/nvim/after/ftplugin/ruby.lua
@@ -0,0 +1,8 @@
+-- Include `!` and `?` when deleting words
+vim.opt_local.iskeyword = vim.opt_local.iskeyword + "!" + "?"
+
+-- Open `ri` in a terminal
+vim.opt_local.keywordprg = ":term ri"
+
+-- Do not add `#` on a new line when hitting `o` on a commented line
+vim.opt_local.formatoptions = vim.opt_local.formatoptions - 'o'
--- .config/nvim/after/ftplugin/ruby.vim
+++
@@ -1,8 +0,0 @@
-" Include `!` and `?` when deleting words
-setlocal iskeyword+=!,?
-
-" Open `ri` in a terminal
-setlocal keywordprg=:term\ ri
-
-" Do not add `#` on a new line when hitting `o` on a commented line
-setlocal formatoptions-=o
