09442ce23a3a2de9cfe87ec02e98579762e82561
Mathias Jean Johansen <mathias@mjj.io>
2021-07-12 21:25:12 +0200

    Convert `erlang.vim` to Lua.

---
+++ .config/nvim/after/ftplugin/erlang.lua
@@ -0,0 +1,5 @@
+-- Do not add `%` on a new line when hitting `o` on a commented line
+vim.opt_local.formatoptions = vim.opt_local.formatoptions - 'o'
+
+-- Use %% for comments instead of a single %
+vim.opt_local.commentstring = "%% %s"
--- .config/nvim/after/ftplugin/erlang.vim
+++
@@ -1,5 +0,0 @@
-" Do not add `%` on a new line when hitting `o` on a commented line
-setlocal formatoptions-=o
-
-" Use %% for comments instead of a single %
-setlocal commentstring=%%\ %s
