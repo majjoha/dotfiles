d25429cf3f9266e6447b5fe18059283918ff193a
Mathias Jean Johansen <mathias@mjj.io>
2021-07-15 12:01:40 +0200

    Refactor status line.

--- .config/nvim/plugin/statusline.lua
+++ .config/nvim/plugin/statusline.lua
@@ -1,29 +1,22 @@
-local majjoha = require("majjoha")
+local show_git_branch = require("majjoha").show_git_branch
 
--- Flush status line
-vim.opt.statusline = ""
-
--- Show the current Git branch
-vim.opt.statusline = vim.o.statusline .. majjoha.show_git_branch() .. " "
-
--- Show the name of the current file
-vim.opt.statusline = vim.o.statusline .. "%<%f "
-
--- Show modified flag
-vim.opt.statusline = vim.o.statusline .. "%-4m"
-
--- Show readonly flag
-vim.opt.statusline = vim.o.statusline .. "%r"
-
--- Align right
-vim.opt.statusline = vim.o.statusline .. "%="
-
--- Show the file type if any file type is set
-vim.opt.statusline = vim.o.statusline ..
-                       "%{strlen(&filetype)>0?&filetype.' | ':''}"
-
--- Show the percentage through the file
-vim.opt.statusline = vim.o.statusline .. "%4.(%p%%%) "
-
--- Show line and column numbers
-vim.opt.statusline = vim.o.statusline .. "| %6.(%l:%c%) "
+-- LuaFormatter off
+vim.opt.statusline = table.concat({
+  -- Show the current Git branch
+  show_git_branch() .. " ",
+  -- Show the name of the current file
+  "%<%f ",
+  -- Show modified flag
+  "%-4m",
+  -- Show readonly flag
+  "%r",
+  -- Align right
+  "%=",
+  -- Show the file type if any file type is set
+  "%{strlen(&filetype)>0?&filetype.' | ':''}",
+  -- Show the percentage through the file
+  "%4.(%p%%%) ",
+  -- Show line and column numbers
+  "| %6.(%l:%c%) "
+})
+-- LuaFormatter on
