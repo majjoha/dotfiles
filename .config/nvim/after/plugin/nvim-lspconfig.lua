ea349a071348b5f7adb8e8c2fdebbec07f3f161b
Mathias Jean Johansen <mathias@mjj.io>
2022-03-06 11:31:07 +0100

    Define custom border for floating windows.

--- .config/nvim/after/plugin/nvim-lspconfig.lua
+++ .config/nvim/after/plugin/nvim-lspconfig.lua
@@ -1,5 +1,24 @@
 local lspconfig = require("lspconfig")
 
+local border = {
+  { "┌", "FloatBorder" }, { "─", "FloatBorder" }, { "┐", "FloatBorder" },
+  { "│", "FloatBorder" }, { "┘", "FloatBorder" }, { "─", "FloatBorder" },
+  { "└", "FloatBorder" }, { "│", "FloatBorder" },
+}
+
+-- Use custom border on hover
+vim.lsp.handlers["textDocument/hover"] =
+  vim.lsp.with(vim.lsp.handlers.hover, { border = border })
+
+-- Override `vim.lsp.util.open_floating_preview` function so it uses the custom
+-- border instead
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 local on_attach = function(client)
   -- Enable completion through LSP servers
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
