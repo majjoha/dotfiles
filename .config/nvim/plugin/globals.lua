c028f4fac3655000868ddacd5b7f2edafbad84cb
Mathias Jean Johansen <mathias@mjj.io>
2022-03-06 11:45:25 +0100

    Add function for viewing definitions via LSP.

--- .config/nvim/plugin/globals.lua
+++ .config/nvim/plugin/globals.lua
@@ -18,3 +18,19 @@ function _G.fzf_search_branches()
     options = "+m --prompt='Branch >' --preview='git show --color=always {}'",
   }))
 end
+
+-- Peek definition under cursor via LSP
+function _G.peek_definition()
+  local params = vim.lsp.util.make_position_params()
+
+  function preview_location_callback(_, result)
+    if result == nil or vim.tbl_isempty(result) then
+      return nil
+    end
+
+    vim.lsp.util.preview_location(result[1])
+  end
+
+  return vim.lsp.buf_request(0, "textDocument/definition", params,
+                             preview_location_callback)
+end
