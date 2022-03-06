4b501361103495166cb4f821e76a6e3b8e03e211
Mathias Jean Johansen <mathias@mjj.io>
2022-03-06 11:51:16 +0100

    Configure signs for diagnostics.

--- .config/nvim/after/plugin/nvim-lspconfig.lua
+++ .config/nvim/after/plugin/nvim-lspconfig.lua
@@ -46,6 +46,31 @@ end
 -- Disable virtual text
 vim.diagnostic.config({ virtual_text = false })
 
+-- Define signs for diagnostics
+vim.fn.sign_define("DiagnosticSignError", {
+  text = "",
+  texthl = "DiagnosticSignError",
+  numhl = "DiagnosticSignError",
+})
+
+vim.fn.sign_define("DiagnosticSignWarn", {
+  text = "",
+  texthl = "DiagnosticSignWarn",
+  numhl = "DiagnosticSignWarn",
+})
+
+vim.fn.sign_define("DiagnosticSignHint", {
+  text = "",
+  texthl = "DiagnosticSignHint",
+  numhl = "DiagnosticSignHint",
+})
+
+vim.fn.sign_define("DiagnosticSignInfo", {
+  text = "",
+  texthl = "DiagnosticSignInfo",
+  numhl = "DiagnosticSignInfo",
+})
+
 local on_attach = function(client)
   -- Enable completion through LSP servers
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
