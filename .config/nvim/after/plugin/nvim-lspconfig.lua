96b79d18e94a17e4dc301ed7ee0553ff060c2b4a
Mathias Jean Johansen <mathias@mjj.io>
2022-03-06 11:00:02 +0100

    Set up `efm-langserver`.

---
+++ .config/nvim/after/plugin/nvim-lspconfig.lua
@@ -0,0 +1,47 @@
+local lspconfig = require("lspconfig")
+
+local on_attach = function(client)
+  if client.resolved_capabilities.document_formatting then
+    local autocmd = [[
+      augroup lsp_formatting
+        autocmd!
+        autocmd BufWritePre <buffer> :lua vim.lsp.buf.formatting_seq_sync()
+      augroup END
+    ]]
+
+    vim.cmd(autocmd)
+  end
+end
+
+local languages = {
+  javascript = {
+    {
+      formatCommand = "./node_modules/.bin/prettier --stdin-filepath ${INPUT}",
+      formatStdin = true,
+    },
+  },
+  json = { { formatCommand = "jq .", formatstdin = true } },
+  lua = { { formatCommand = "lua-format -i", formatStdin = true } },
+  sh = {
+    {
+      lintCommand = "shellcheck -f gcc -x -",
+      lintSource = "shellcheck",
+      lintStdin = true,
+      lintFormats = { "%f:%l:%c: %t%*[^:]: %m [SC%n]" },
+    },
+    { formatCommand = "shfmt -ln bash -i 2 -bn -ci -sr -kp", formatStdin = true },
+  },
+  yaml = {
+    {
+      lintCommand = "yamllint --strict --format parsable ${INPUT}",
+      lintStdin = false,
+      lintFormats = { "%f:%l:%c: [%t%*[a-z]] %m" },
+    },
+  },
+}
+
+lspconfig.efm.setup({
+  on_attach = on_attach,
+  init_options = { documentFormatting = true },
+  settings = { languages = languages, filetypes = vim.tbl_keys(languages) },
+})
