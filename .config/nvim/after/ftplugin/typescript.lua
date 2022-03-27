87a1905f5f65a96040e4fc8d1088c480d639495c
Mathias Jean Johansen <mathias@mjj.io>
2022-03-27 09:27:16 +0200

    Remove `ALEGoToDefinition` mapping.

--- .config/nvim/after/ftplugin/typescript.lua
+++ .config/nvim/after/ftplugin/typescript.lua
@@ -1,6 +1,2 @@
 -- Do not add `//` on a new line when hitting `o` on a commented line
 vim.opt_local.formatoptions = vim.opt_local.formatoptions - 'o'
-
--- Use ALE to move to definitions
-vim.api.nvim_buf_set_keymap(0, "n", "<C-]>", ":ALEGoToDefinition<CR>",
-                            { noremap = true, silent = true })
