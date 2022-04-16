0a8c75ab8c5d77d1a749c90ad0c061cf5b89f8a7
Mathias Jean Johansen <mathias@mjj.io>
2022-04-16 14:52:01 +0200

    Replace `filetype.lua` with `filetype.vim`.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
 -- Add FZF to the `runtimepath`
 vim.opt.runtimepath = vim.o.runtimepath ..
                         ',/usr/local/opt/fzf,/opt/homebrew/opt/fzf/'
+
+-- Use only `filetype.lua` and not `filetype.vim`
+vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
