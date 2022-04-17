bb3ac95ac620542053950d5384872ff4d06f8024
Mathias Jean Johansen <mathias@mjj.io>
2022-04-17 13:39:55 +0200

    Move Git branch information to tmux statusline.

--- .config/nvim/plugin/statusline.lua
+++ .config/nvim/plugin/statusline.lua
@@ -1,20 +1,7 @@
-function show_git_branch()
-  local branchname = vim.fn["fugitive#statusline"]()
-
-  if string.len(branchname) > 0 then
-    local formatted_branchname = string.sub(branchname, 6):sub(1, -3)
-    return "   " .. formatted_branchname .. "  "
-  else
-    return " "
-  end
-end
-
 -- LuaFormatter off
 vim.opt.statusline = table.concat({
-  -- Show the current Git branch
-  "%{luaeval('show_git_branch()')}",
   -- Show the name of the current file
-  "%<%f ",
+  " %<%f ",
   -- Show modified flag
   "%-4m",
   -- Show readonly flag
