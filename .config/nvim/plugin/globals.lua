769293713134f7e483f0be74f376433da422bd33
Mathias Jean Johansen <mathias@mjj.io>
2022-04-18 11:12:29 +0200

    Remove unused global functions.

--- .config/nvim/plugin/globals.lua
+++ .config/nvim/plugin/globals.lua
@@ -1,15 +1,3 @@
--- Utility function for escaping terminal keycodes
--- Source: https://github.com/nanotee/nvim-lua-guide#vimapinvim_replace_termcodes
-function _G.t(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
--- Use `<Esc>` to return to normal mode in terminals unless the terminal is
--- running FZF
-function _G.smart_esc()
-  return vim.bo.filetype == "fzf" and t("<Esc>") or t("<C-\\><C-n>")
-end
-
 -- Search branches using FZF
 function _G.fzf_search_branches()
   vim.fn["fzf#run"](vim.fn["fzf#wrap"]({
