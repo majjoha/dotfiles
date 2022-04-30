6f2c9f0ff217f0a93d94068deedbbf373e9fb3ce
Mathias Jean Johansen <mathias@mjj.io>
2022-04-30 09:13:58 +0200

    Fix conflicting `<Leader>x` key binding.

--- .config/nvim/after/plugin/nvim-snippy.lua
+++ .config/nvim/after/plugin/nvim-snippy.lua
@@ -3,6 +3,6 @@ local snippy = require("snippy")
 snippy.setup({
   mappings = {
     is = { ["<Tab>"] = "expand_or_advance", ["<S-Tab>"] = "previous" },
-    nx = { ["<leader>x"] = "cut_text" },
+    nx = { ["<leader>z"] = "cut_text" },
   },
 })
