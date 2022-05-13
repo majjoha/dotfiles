412ac1a3924afe4e57abd48bd0ade85cecdd1f30
Mathias Jean Johansen <mathias@mjj.io>
2022-05-13 13:58:50 +0200

    Add configuration for `fzf-lua`.

---
+++ .config/nvim/after/plugin/fzf-lua.lua
@@ -0,0 +1,35 @@
+require("fzf-lua").setup({
+  winopts = {
+    height = 0.7,
+    width = 0.9,
+    border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+    preview = {
+      horizontal = "right:50%",
+      title = false,
+    }
+  },
+  fzf_opts = {
+    ["--inline-info"] = "",
+    ["--reverse"] = "",
+  },
+  fzf_colors = {
+    bg = { "bg", "Normal" },
+    fg = { "fg", "Normal" },
+    hl = { "fg", "StatusLineNC" },
+    ["bg+"] = { "bg", "StatusLine" },
+    ["fg+"] = { "fg", "Normal" },
+    ["hl+"] = { "fg", "StatusLine" },
+    border = { "fg", "LineNr" },
+    gutter = { "bg", "Normal" },
+    header = { "fg", "Comment" },
+    info = { "fg", "StatusLine" },
+    marker = { "fg", "Normal" },
+    pointer = { "fg", "Normal" },
+    prompt = { "fg", "LineNr" },
+    spinner = { "fg", "Normal" },
+  },
+  grep = {
+    rg_opts =
+      "--column --line-number --no-heading --color=always --smart-case --hidden",
+  }
+})
