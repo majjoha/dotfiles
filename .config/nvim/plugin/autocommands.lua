02b54bb57b9b4261434a152986d4ab16ba2cd6c6
Mathias Jean Johansen <mathias@mjj.io>
2022-10-02 10:52:48 +0200

    Assign `augroup`s to variables.

--- .config/nvim/plugin/autocommands.lua
+++ .config/nvim/plugin/autocommands.lua
@@ -1,25 +1,26 @@
 -- Resize splits when the window is resized
-vim.api.nvim_create_augroup("ResizeSplits", {})
+local resize_splits_group = vim.api.nvim_create_augroup("ResizeSplits", {})
 vim.api.nvim_create_autocmd("VimResized", {
   pattern = "*",
   command = "wincmd =",
-  group = "ResizeSplits",
+  group = resize_splits_group,
 })
 
 -- Auto-clean Fugitive buffers
 -- Source: http://vimcasts.org/episodes/fugitive-vim-browsing-the-git-object-database/
-vim.api.nvim_create_augroup("CleanFugitiveBuffers", {})
+local clean_fugitive_buffers_group = vim.api.nvim_create_augroup(
+                                       "CleanFugitiveBuffers", {})
 vim.api.nvim_create_autocmd("BufReadPost", {
   pattern = "fugitive://*",
   callback = function()
     vim.opt.bufhidden = "delete"
   end,
-  group = "CleanFugitiveBuffers",
+  group = clean_fugitive_buffers_group,
 })
 
 -- Remove line numbers in terminal mode, start it in insert mode and delete the
 -- buffer when it is no longer displayed
-vim.api.nvim_create_augroup("TerminalMode", {})
+local terminal_mode_group = vim.api.nvim_create_augroup("TerminalMode", {})
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "*",
   callback = function()
@@ -29,19 +30,20 @@ vim.api.nvim_create_autocmd("TermOpen", {
     vim.wo.statusline = " "
     vim.api.nvim_command("startinsert")
   end,
-  group = "TerminalMode",
+  group = terminal_mode_group,
 })
 vim.api.nvim_create_autocmd("TermLeave", {
   pattern = "term://*",
   callback = function()
     vim.opt.bufhidden = "delete"
   end,
-  group = "TerminalMode",
+  group = terminal_mode_group,
 })
 
 -- Customize colors
 -- See https://gist.github.com/romainl/379904f91fa40533175dfaec4c833f2f
-vim.api.nvim_create_augroup("CustomizeColors", {})
+local customize_colors_group = vim.api
+                                 .nvim_create_augroup("CustomizeColors", {})
 vim.api.nvim_create_autocmd("ColorScheme", {
   pattern = "*",
   callback = function()
@@ -49,7 +51,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "LineNr", { ctermbg = 00 })
 
     -- Remove background color for fold column
-    vim.api.nvim_set_hl(0, "FoldColumn", { link = "Normal"})
+    vim.api.nvim_set_hl(0, "FoldColumn", { link = "Normal" })
 
     -- Set the color of the vertical split bar to transparent
     vim.api.nvim_set_hl(0, "VertSplit", { ctermbg = 00 })
@@ -65,7 +67,8 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     -- Set the sign column to have a transparent background and define the
     -- foreground colors for the various signs
     vim.api.nvim_set_hl(0, "SignColumn", { ctermbg = 00 })
-    vim.api.nvim_set_hl(0, "DiagnosticSignError", { ctermfg = 01, ctermbg = 00 })
+    vim.api
+      .nvim_set_hl(0, "DiagnosticSignError", { ctermfg = 01, ctermbg = 00 })
     vim.api.nvim_set_hl(0, "DiagnosticSignWarn", { ctermfg = 03, ctermbg = 00 })
     vim.api.nvim_set_hl(0, "DiagnosticSignHint", { ctermfg = 07, ctermbg = 00 })
     vim.api.nvim_set_hl(0, "DiagnosticSignInfo", { ctermfg = 07, ctermbg = 00 })
@@ -75,8 +78,9 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "FloatBorder", { ctermbg = 00 })
 
     -- Hide status line for NvimTree buffer
-    vim.api.nvim_set_hl(0, "NvimTreeStatusLineNC", { ctermbg = 00, ctermfg = 00 })
+    vim.api.nvim_set_hl(0, "NvimTreeStatusLineNC",
+                        { ctermbg = 00, ctermfg = 00 })
     vim.api.nvim_set_hl(0, "NvimTreeStatusLine", { ctermbg = 00, ctermfg = 00 })
   end,
-  group = "CustomizeColors",
+  group = customize_colors_group,
 })
