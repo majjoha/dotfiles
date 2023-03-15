68a373ca5a8f6b7e74cdbfecc555c465b9337466
Mathias Jean Johansen <mathias@mjj.io>
2023-03-15 19:03:01 +0100

    Configure test wrapper for HSpec.

--- .config/nvim/after/plugin/nvim-test.lua
+++ .config/nvim/after/plugin/nvim-test.lua
@@ -5,3 +5,11 @@ require("nvim-test").setup({
     stopinsert = "false"
   }
 })
+
+require("nvim-test.runners.hspec"):setup({
+  command = "hspec-test-wrapper",
+  args = { "" },
+  file_pattern = "\\v(Spec)\\.hs$",
+  find_files = { "{name}Spec.hs", "Spec.hs" },
+  filename_modifier = "",
+})
