3b84dc375cc4491091b7266d993a41e76ba969ae
Mathias Jean Johansen <mathias@mjj.io>
2023-07-15 08:29:03 +0200

    Fix LSP syntax highlighting.

--- .config/nvim/plugin/autocommands.lua
+++ .config/nvim/plugin/autocommands.lua
@@ -105,6 +105,26 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 
     -- Set color for virtual color column
     vim.api.nvim_set_hl(0, "VirtColumn", { ctermfg = 10 })
+
+    local links = {
+      ['@lsp.type.namespace'] = '@namespace',
+      ['@lsp.type.type'] = '@type',
+      ['@lsp.type.class'] = '@type',
+      ['@lsp.type.enum'] = '@type',
+      ['@lsp.type.interface'] = '@type',
+      ['@lsp.type.struct'] = '@structure',
+      ['@lsp.type.parameter'] = '@parameter',
+      ['@lsp.type.variable'] = '@variable',
+      ['@lsp.type.property'] = '@property',
+      ['@lsp.type.enumMember'] = '@constant',
+      ['@lsp.type.function'] = '@function',
+      ['@lsp.type.method'] = '@method',
+      ['@lsp.type.macro'] = '@macro',
+      ['@lsp.type.decorator'] = '@function',
+    }
+    for newgroup, oldgroup in pairs(links) do
+      vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+    end
   end,
   group = customize_colors_group,
 })
