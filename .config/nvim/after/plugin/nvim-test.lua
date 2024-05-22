38549af6dff0d9c1d64d23be383e28f0b8e67fb6
Mathias Jean Johansen <mathias@mjj.io>
2024-05-22 07:37:44 +0200

    Remove HSpec test runner.

--- .config/nvim/after/plugin/nvim-test.lua
+++ .config/nvim/after/plugin/nvim-test.lua
@@ -1,11 +1,3 @@
 require("nvim-test").setup({
   termOpts = { direction = "vertical", width = 90, stopinsert = "false" },
 })
-
-require("nvim-test.runners.hspec"):setup({
-  command = "hspec-test-wrapper",
-  args = { "" },
-  file_pattern = "\\v(Spec)\\.hs$",
-  find_files = { "{name}Spec.hs", "Spec.hs" },
-  filename_modifier = "",
-})
