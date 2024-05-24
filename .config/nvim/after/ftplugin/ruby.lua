9a699235ae2b4fddddec6eb9ca33cbdea6ab273b
Mathias Jean Johansen <mathias@mjj.io>
2024-05-24 08:08:31 +0200

    Use Ruby LSP for looking up definitions.

--- .config/nvim/after/ftplugin/ruby.lua
+++ .config/nvim/after/ftplugin/ruby.lua
@@ -2,7 +2,7 @@
 vim.opt_local.iskeyword = vim.opt_local.iskeyword + "!" + "?"
 
 -- Open `ri` in a terminal
-vim.opt_local.keywordprg = ":tabe | term ri"
+-- vim.opt_local.keywordprg = ":tabe | term ri"
 
 -- Do not add `#` on a new line when hitting `o` on a commented line
 vim.opt_local.formatoptions = vim.opt_local.formatoptions - 'o'
