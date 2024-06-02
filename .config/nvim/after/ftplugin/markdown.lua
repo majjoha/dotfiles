9e32c4b4ef33dde79962ca79c38dd5cd0113ad03
Mathias Jean Johansen <mathias@mjj.io>
2024-06-02 08:55:53 +0200

    Use two spaces for tabs in Markdown files.

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -8,3 +8,9 @@ vim.opt_local.textwidth = 80
 
 -- Enable completion
 vim.opt_local.complete = vim.opt_local.complete + "kspell"
+
+-- Use two spaces for tabs in Markdown files
+vim.opt_local.expandtab = true
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
