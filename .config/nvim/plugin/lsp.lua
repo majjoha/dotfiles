856a477d44cc7a7601433f9bb7e1fe4a3aebcd7a
Mathias Jean Johansen <mathias@mjj.io>
2025-03-27 16:09:43 +0100

    Move settings for diagnostics to `.config/nvim/plugin/lsp.lua`.

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,3 +1,27 @@
+-- Define signs for diagnostics
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "▍",
+      [vim.diagnostic.severity.WARN] = "▍",
+      [vim.diagnostic.severity.HINT] = "▍",
+      [vim.diagnostic.severity.INFO] = "▍",
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+      [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+      [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+      [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+      [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+      [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+      [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+    },
+  },
+})
+
 local configurations = {}
 
 for _, v in ipairs(vim.api.nvim_get_runtime_file("lsp/*", true)) do
