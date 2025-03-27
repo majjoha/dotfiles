b4360ae6d4a6f5e11bb46d537e9d60e529357c13
Mathias Jean Johansen <mathias@mjj.io>
2025-03-27 11:48:19 +0100

    Remove LSP server configurations.

--- .config/nvim/after/plugin/nvim-lspconfig.lua
+++ .config/nvim/after/plugin/nvim-lspconfig.lua
@@ -21,13 +21,3 @@ vim.diagnostic.config({
     },
   },
 })
-
-local servers = {
-}
-
-for server, config in pairs(servers) do
-  require("lspconfig")[server].setup(vim.tbl_deep_extend("force", {
-    init_options = { documentFormatting = true },
-    flags = { debounce_text_changes = 150 },
-  }, config))
-end
