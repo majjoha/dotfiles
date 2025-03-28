bceba24842288544a9cdcc56839c6847722b7a85
Mathias Jean Johansen <mathias@mjj.io>
2025-03-28 11:52:41 +0100

    Remove unnecessary `commentstring` setting.

--- .config/nvim/after/ftplugin/gleam.lua
+++ .config/nvim/after/ftplugin/gleam.lua
@@ -1,5 +1,2 @@
 -- Do not add `//` on a new line when hitting `o` on a commented line
 vim.opt_local.formatoptions = vim.opt_local.formatoptions - 'o'
-
--- Use `//` for comments
-vim.opt_local.commentstring = "// %s"
