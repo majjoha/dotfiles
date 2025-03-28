ccd371e0a367ae1a181148257a0c2fba3a34c63b
Mathias Jean Johansen <mathias@mjj.io>
2025-03-28 07:05:02 +0100

    Replace `mini.comment` with commenting feature in Neovim 0.11.

--- .config/nvim/after/plugin/mini.lua
+++ .config/nvim/after/plugin/mini.lua
@@ -1,17 +1,5 @@
 require("mini.bracketed").setup({})
 
-require("mini.comment").setup({
-  mappings = {
-    -- Toggle comment (like `gcip` - comment inner paragraph) for both
-    -- Normal and Visual modes
-    comment = "<Leader>/",
-    -- Toggle comment on current line
-    comment_line = "<Leader>/",
-    -- Define "comment" textobject (like `dgc` - delete whole comment block)
-    textobject = "gc",
-  },
-})
-
 require("mini.files").setup({
   mappings = {
     go_in = "<Tab>",
