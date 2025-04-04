9e6e75b22faf7c847e5b3f1ead9eddd7d26e4fc3
Mathias Jean Johansen <mathias@mjj.io>
2025-04-04 09:37:58 +0200

    Change text for diagnostic signs.

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -2,10 +2,10 @@
 vim.diagnostic.config({
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = "▍",
-      [vim.diagnostic.severity.WARN] = "▍",
-      [vim.diagnostic.severity.HINT] = "▍",
-      [vim.diagnostic.severity.INFO] = "▍",
+      [vim.diagnostic.severity.ERROR] = "░",
+      [vim.diagnostic.severity.WARN] = "░",
+      [vim.diagnostic.severity.HINT] = "░",
+      [vim.diagnostic.severity.INFO] = "░",
     },
     linehl = {
       [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
