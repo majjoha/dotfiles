23f662e72902d53d24854723f004fe5148a8a68a
Mathias Jean Johansen <mathias@mjj.io>
2025-09-21 16:10:41 +0200

    Remove `vim-marginalia`.

--- .config/nvim/after/plugin/vim-marginalia.lua
+++
@@ -1,7 +0,0 @@
--- Skip if Marginalia is not loaded
-if not vim.g.loaded_marginalia then
-  return
-end
-
-vim.g.marginalia_notes_path = vim.fn.expand(
-                                "~/Dropbox/Tekstdokumenter/Wiki/notes")
