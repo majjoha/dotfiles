b663cc12def9597f302321a8051a560513f3e048
Mathias Jean Johansen <mathias@mjj.io>
2025-10-28 20:35:24 +0100

    Load snippets from `~/.config/nvim/after/snippets`.

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -1,6 +1,8 @@
 require("luasnip").setup({})
 
-require("luasnip.loaders.from_snipmate").lazy_load()
-require("luasnip.loaders.from_snipmate").lazy_load(
-  { paths = "~/.config/nvim/pack/packages/start/vim-snippets/snippets" }
-)
+require("luasnip.loaders.from_snipmate").lazy_load({
+  paths = {
+    "~/.config/nvim/pack/packages/start/vim-snippets/snippets",
+    "~/.config/nvim/after/snippets",
+  },
+})
