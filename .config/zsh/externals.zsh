25e5aa548e3fe47a3aee05251e6300c114323d3b
Mathias Jean Johansen <mathias@mjj.io>
2026-01-02 11:28:06 +0100

    Guard against uninstalled tools.

--- .config/zsh/externals.zsh
+++ .config/zsh/externals.zsh
@@ -1,5 +1,7 @@
 # Use z.lua
-eval "$(lua "$HOME"/.bin/z.lua/z.lua --init zsh)"
+if command -v lua >/dev/null 2>&1 && [ -f "$HOME/.bin/z.lua/z.lua" ]; then
+  eval "$(lua "$HOME"/.bin/z.lua/z.lua --init zsh)"
+fi
 
 # ghcup-env
 [ -f "$HOME/.ghcup/env" ] && source "$HOME"/.ghcup/env
@@ -8,7 +10,11 @@ eval "$(lua "$HOME"/.bin/z.lua/z.lua --init zsh)"
 [ -f "$HOME/.cargo/env" ] && source "$HOME"/.cargo/env
 
 # Use mise
-eval "$(mise activate zsh)"
+if command -v mise >/dev/null 2>&1; then
+  eval "$(mise activate zsh)"
+fi
 
 # Use FZF
-source <(fzf --zsh)
+if command -v fzf >/dev/null 2>&1; then
+  source <(fzf --zsh)
+fi
