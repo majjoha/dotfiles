f9426c7931acc73c271ac0aef7a1351a58a2c43c
Mathias Jean Johansen <mathias@mjj.io>
2026-01-04 10:23:48 +0100

    Remove references of `lua-format`.

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,5 +1,5 @@
--- Use `lua-format` for formatting
-vim.opt_local.formatprg = "lua-format -i"
+-- Use `stylua` for formatting
+vim.opt_local.formatprg = "stylua -"
 
 -- Do not add `--` on a new line when hitting `o` on a commented line
 vim.opt_local.formatoptions = vim.opt_local.formatoptions - 'o'
