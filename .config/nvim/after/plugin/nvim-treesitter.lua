7152a5fabbd603adacc982abb2e21480e6a89c59
Mathias Jean Johansen <mathias@mjj.io>
2026-01-11 18:08:52 +0100

    Install Treesitter parsers on demand instead.

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = "all",
-  ignore_install = { "phpdoc" },
+  ensure_installed = {},
+  auto_install = true,
   highlight = {
     enable = true,
     disable = {},
