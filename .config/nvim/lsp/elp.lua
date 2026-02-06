cb6e82696ac99a5d39362b6e7865ba8644344673
Mathias Jean Johansen <mathias@mjj.io>
2026-02-06 14:41:13 +0100

    Replace `erlang_ls` with `elp`.

---
+++ .config/nvim/lsp/elp.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = { "elp", "server" },
+  filetypes = { "erlang" },
+  root_markers = { "rebar.config", "erlang.mk", ".git" },
+}
--- .config/nvim/lsp/erlang-ls.lua
+++
@@ -1,6 +0,0 @@
-return {
-  cmd = { "erlang_ls" },
-  root_markers = { "rebar.config", "erlang.mk", ".git" },
-  filetypes = { "erlang" },
-  single_file_support = true,
-}
