fa82ed280bfd7e5892ad868b1ebc7cf830b9e4c2
Mathias Jean Johansen <mathias@mjj.io>
2026-02-06 07:07:35 +0100

    Fix command for `elixir-lsp`.

--- .config/nvim/lsp/elixirls.lua
+++ .config/nvim/lsp/elixirls.lua
@@ -1,5 +1,5 @@
 return {
-  cmd = { "elixir-ls" },
+  cmd = { "language_server.sh" },
   root_markers = { "mix.exs" },
   filetypes = { "elixir", "eelixir", "heex", "surface" },
   single_file_support = true,
