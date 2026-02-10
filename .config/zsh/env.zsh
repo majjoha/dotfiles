3e015dd311c62df90439736f761d37d0f93767d6
Mathias Jean Johansen <mathias@mjj.io>
2026-02-10 06:49:03 +0100

    Remove preview from `FZF_DEFAULT_OPTS`.

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -47,16 +47,17 @@ export GPG_TTY=$(tty)
 # Show hidden files when searching
 export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git/*'"
 
-# Show the prompt at the top with inline info, place a border around FZF and
-# take up only 35 percent of the screen height. Display a preview of the file,
-# and set the colors to match the terminal theme
+# Show the prompt at the top with inline info, place a border around
+# FZF and take up only 35 percent of the screen height, and set the
+# colors to match the terminal theme
 export FZF_DEFAULT_OPTS="
   --reverse \
   --height=50% \
   --style minimal \
   --border sharp \
-  --preview='bat --style=numbers --color=always --line-range :200 {}' \
-  --color=bg+:11,bg:0,border:8,spinner:7,hl:8,fg:7,prompt:8,header:8,info:12,pointer:7,marker:7,fg+:7,gutter:0,hl+:12"
+  --color=bg+:11,bg:0,border:8,spinner:7,hl:8,\
+fg:7,prompt:8,header:8,info:12,pointer:7,\
+marker:7,fg+:7,gutter:0,hl+:12"
 
 # Use Neovim when viewing manual pages
 export MANPAGER="nvim +Man!"
