1ac00f3efa4c9bf94c541be6eadb5c79392e9271
Mathias Jean Johansen <mathias@mjj.io>
2026-03-10 10:17:26 +0100

    Remove `formatprg` for Lua in favor of LSP.

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,5 +1,2 @@
--- Use `stylua` for formatting
-vim.opt_local.formatprg = "stylua -"
-
 -- Do not add `--` on a new line when hitting `o` on a commented line
 vim.opt_local.formatoptions = vim.opt_local.formatoptions - 'o'
