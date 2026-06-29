0d4ba1429420b128f2746a49f308ffb666d57fcf
Mathias Jean Johansen <mathias@mjj.io>
2026-06-29 14:27:45 +0200

    Replace single quotes with double quotes.

--- .config/nvim/after/ftplugin/typescript.lua
+++ .config/nvim/after/ftplugin/typescript.lua
@@ -1,2 +1,2 @@
 -- Do not add `//` on a new line when hitting `o` on a commented line
-vim.opt_local.formatoptions = vim.opt_local.formatoptions - 'o'
+vim.opt_local.formatoptions = vim.opt_local.formatoptions - "o"
